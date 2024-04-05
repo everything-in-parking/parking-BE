3dd63ca6c75dcfb5e62e84cf2ff5d68ea9427e8d
youngh0 <cyh6099@gmail.com>
2024-04-05 19:53:35 +0900

    주차장 상세조회 session 검사 interceptor 제거 (#65)
    * feat: 주차장 상세조회 session 검사 interceptor 제거
    
    * feat: 불필요한 interceptor 제거

--- src/main/java/com/example/parking/config/WebMvcConfig.java
+++ src/main/java/com/example/parking/config/WebMvcConfig.java
@@ -36,7 +36,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
                         "/swagger-ui/**",
                         "/signup",
                         "/signin",
-                        "/parkings"
+                        "/parkings/**"
                 ));
     }
 
