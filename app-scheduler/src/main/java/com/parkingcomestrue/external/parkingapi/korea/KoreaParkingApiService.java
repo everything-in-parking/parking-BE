b107679d7430b308e597034dab8570fc23667f38
This2sho <zx00018@naver.com>
2024-05-07 18:10:51 +0900

    refactor: 요청 url 및 URI 생성 방식 변경

--- app-scheduler/src/main/java/com/parkingcomestrue/external/parkingapi/korea/KoreaParkingApiService.java
+++ app-scheduler/src/main/java/com/parkingcomestrue/external/parkingapi/korea/KoreaParkingApiService.java
@@ -3,28 +3,22 @@ package com.parkingcomestrue.external.parkingapi.korea;
 import com.parkingcomestrue.common.domain.parking.Parking;
 import com.parkingcomestrue.external.parkingapi.ParkingApiService;
 import java.net.URI;
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Component
 public class KoreaParkingApiService implements ParkingApiService {
 
-    private static final String URL = "http://apis.data.go.kr/6260000/BusanPblcPrkngInfoService/getPblcPrkngInfo";
+    private static final String URL = "http://api.data.go.kr/openapi/tn_pubr_prkplce_info_api";
     private static final String RESULT_TYPE = "json";
-    private static final int SIZE = 1000;
+    private static final int SIZE = 250;
     private static final String NORMAL_RESULT_CODE = "00";
 
     @Value("${korea-parking-key}")
@@ -58,23 +52,18 @@ public class KoreaParkingApiService implements ParkingApiService {
 
     private KoreaParkingResponse call(int startIndex, int size) {
         URI uri = makeUri(startIndex, size);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        HttpEntity<String> entity = new HttpEntity<String>(headers);
-        ResponseEntity<KoreaParkingResponse> response = restTemplate.exchange(uri, HttpMethod.GET, entity,
-                KoreaParkingResponse.class);
+        ResponseEntity<KoreaParkingResponse> response = restTemplate.getForEntity(uri, KoreaParkingResponse.class);
         return response.getBody();
     }
 
     private URI makeUri(int startIndex, int size) {
-        return UriComponentsBuilder
-                .fromHttpUrl(URL)
+        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(URL);
+        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+        return factory.builder()
                 .queryParam("serviceKey", API_KEY)
                 .queryParam("pageNo", startIndex)
                 .queryParam("numOfRows", size)
                 .queryParam("type", RESULT_TYPE)
-                .encode(StandardCharsets.UTF_8)
-                .build()
-                .toUri();
+                .build();
     }
 }
