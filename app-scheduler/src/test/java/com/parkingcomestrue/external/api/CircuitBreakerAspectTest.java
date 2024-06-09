90d5d671feb229b2adb9a34f8ad61f58d55e85f5
This2sho <zx00018@naver.com>
2024-06-09 20:15:44 +0900

    test: thread sleep 대신 future get 사용

--- app-scheduler/src/test/java/com/parkingcomestrue/external/api/CircuitBreakerAspectTest.java
+++ app-scheduler/src/test/java/com/parkingcomestrue/external/api/CircuitBreakerAspectTest.java
@@ -1,6 +1,11 @@
 package com.parkingcomestrue.external.api;
 
 import com.parkingcomestrue.fake.CircuitBreakerTestService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +22,6 @@ class CircuitBreakerAspectTest {
     private CircuitBreakerTestService service;
 
     private boolean[] isExecuted = {false, false};
-    private final long SECOND = 1000;
 
     @Test
     void 서비스에_에러가_특정_지수를_넘으면_요청이_잠긴다() {
@@ -30,14 +34,14 @@ class CircuitBreakerAspectTest {
         }
 
         //when
-        service.call(() -> {isExecuted[0] = true;});
+        service.call(() -> isExecuted[0] = true);
 
         //then
         Assertions.assertThat(isExecuted[0]).isFalse();
     }
 
     @Test
-    void 서비스가_잠긴후_특정시간이_지나면_다시_요청을_보낼수있다() throws InterruptedException {
+    void 서비스가_잠긴후_특정시간이_지나면_다시_요청을_보낼수있다() throws ExecutionException, InterruptedException {
         //given
         for (int i = 0; i < 8; i++) {
             service.call(() -> {});
@@ -45,10 +49,12 @@ class CircuitBreakerAspectTest {
         for (int i = 0; i < 2; i++) {
             service.call(() -> {throw new RuntimeException();});
         }
-        Thread.sleep(2 * SECOND);
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
         //when
-        service.call(() -> {isExecuted[1] = true;});
+        ScheduledFuture<?> future = scheduler.schedule(() -> service.call(() -> isExecuted[1] = true), 2,
+                TimeUnit.SECONDS);
+        future.get();
 
         //then
         Assertions.assertThat(isExecuted[1]).isTrue();
