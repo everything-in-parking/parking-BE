5a0e6cd36705b5ead1cc845b88dde4df2742418a
This2sho <zx00018@naver.com>
2024-06-13 20:41:21 +0900

    refactor: 리뷰 내용 없을 경우 예외 반환

--- app-api/src/main/java/com/parkingcomestrue/parking/application/review/dto/ReviewCreateRequest.java
+++ app-api/src/main/java/com/parkingcomestrue/parking/application/review/dto/ReviewCreateRequest.java
@@ -1,6 +1,8 @@
 package com.parkingcomestrue.parking.application.review.dto;
 
 import com.parkingcomestrue.common.domain.review.Content;
+import com.parkingcomestrue.common.support.exception.DomainException;
+import com.parkingcomestrue.common.support.exception.DomainExceptionInformation;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -8,6 +10,9 @@ import java.util.stream.Collectors;
 public record ReviewCreateRequest(List<String> contents) {
 
     public Set<Content> toContents() {
+        if (contents == null) {
+            throw new DomainException(DomainExceptionInformation.INVALID_CONTENT);
+        }
         return contents.stream()
                 .map(Content::find)
                 .collect(Collectors.toSet());
